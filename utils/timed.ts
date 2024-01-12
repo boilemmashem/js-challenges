721a148d0d080fb1f71236cb11a493c14082d775
Mike Hart <michael-hart@live.com>
2024-01-12 11:43:50 +0000

    Timed util useful func so saved as gist https://gist.github.com/boilemmashem/4f7e280e7599ee6ec17a680e1509a1e8

---
+++ utils/timed.ts
@@ -0,0 +1,18 @@
+/**
+ * Decorates a function so that it console logs how long it takes to run.
+ * @example
+ *     // import timed
+ *     let functionToTime = () => {...does something}
+ *     functionToTime = timed(functionToTime)
+ *     functionToTime() // use as normal
+ * @param func 
+ * @returns 
+ */
+export const timed = (func: Function) => (...args: any): Function => {
+    const start: number = performance.now()
+    const ret: any = func(...args)
+    console.log(
+        `function ${func.name} took ${(performance.now() - start).toFixed(3)}ms`
+    )
+    return ret
+}
