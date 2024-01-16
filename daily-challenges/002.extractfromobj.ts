aef29e00b8bbdbcba3dada3ac132b51a79018037
Mike Hart <michael-hart@live.com>
2024-01-16 13:01:42 +0000

    challenge solution

---
+++ daily-challenges/002.extractfromobj.ts
@@ -0,0 +1,17 @@
+// Extract the values of a given Property From an Array of Objects
+// https://hackernoon.com/top-three-coding-challenges-for-mid-level-javascript-developers
+
+let input = [
+    { name: "John", age: 21, city: "New York" },
+    { name: "Mike", age: 28, city: "Moscow" },
+    { name: "Danny", age: 30, city: "London" },
+    { name: "Lisa", age: 26, city: "Paris" },
+    { name: "Sophie", age: 19, city: "Berlin" },
+  ];
+
+
+const getNames = (arr: {name: string}[]) :string[] => {
+    return arr.map((obj) => obj.name)
+}
+
+console.log(getNames(input))
