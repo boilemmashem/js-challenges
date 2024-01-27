3a15f642e0c3e0ec8f14b5fe03b9269616762ce6
Mike Hart <michael-hart@live.com>
2024-01-27 23:35:19 +0000

    easy peasy

---
+++ random-challenges/006.minstoseconds.ts
@@ -0,0 +1,8 @@
+// Convert given minutes m to seconds
+// https://edabit.com/challenge/8q54MKnRrm89pSLmW
+
+function minsToSecs(mins: number): number {
+	return mins * 60;
+}
+
+console.log(minsToSecs(3));
